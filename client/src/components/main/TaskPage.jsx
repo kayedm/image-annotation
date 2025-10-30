2c975cc360cd1a80b8490b692ca6b61ec3f0c10b
Kayed Mankal <103891908+kayedm@users.noreply.github.com>
2025-10-30 19:44:42 -0400

    feat: added routes to task page to point towards the picture-annotation route

--- client/src/components/main/TaskPage.jsx
+++ client/src/components/main/TaskPage.jsx
@@ -5,7 +5,7 @@ import styles from "../styles/TaskPage.module.css"
 export default function TaskPage () {
     return (
         <div className={styles.taskPage}>
-            <Card color="grey" to="/test-page" title="Test Page"/>
+            <Card color="grey" to="/picture-annotation" title="Picture Annotation"/>
         </div>
     )
 }
