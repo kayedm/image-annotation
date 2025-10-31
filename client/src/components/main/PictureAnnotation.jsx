b08dcd225c14b241a162c92b92db01f583f95af9
Kayed Mankal <103891908+kayedm@users.noreply.github.com>
2025-10-31 02:15:17 -0400

    feat: added reset and submit button for later implementation, added state for hide points, and passed the props into the components ToolMenu and ImagePreview

--- client/src/components/main/PictureAnnotation.jsx
+++ client/src/components/main/PictureAnnotation.jsx
@@ -3,25 +3,31 @@ import ToolMenu from "./picture-annotation/ToolMenu.jsx";
 import styles from "../styles/picture-annotation/PictureAnnotation.module.css";
 import React from "react";
 import AnnotationCanvas from "./picture-annotation/AnnotationCanvas.jsx";
+import {Button} from "react-bootstrap";
 
 export default function PictureAnnotation() {
     const [selectedTool, setSelectedTool] = React.useState(null);
+    const [hidePoints, setHidePoints] = React.useState(false);
     const [points, setPoints] = React.useState([]);
 
     return (
         <div className={styles.container}>
-
-            <div className={styles.leftSide}>
-                <div className={styles.toolMenu}>
-                    <ToolMenu selectedTool={selectedTool} setSelectedTool={setSelectedTool}/></div>
-                <div className={styles.image}>
-                    <ImagePreview points={points} setPoints={setPoints} selectedTool={selectedTool}/></div>
+            <div className={styles.editingArea}>
+                <div className={styles.leftSide}>
+                    <div className={styles.toolMenu}>
+                        <ToolMenu setHidePoints={setHidePoints} selectedTool={selectedTool} setSelectedTool={setSelectedTool}/></div>
+                    <div className={styles.image}>
+                        <ImagePreview hidePoints={hidePoints} setSelectedTool={setSelectedTool} points={points} setPoints={setPoints} selectedTool={selectedTool}/></div>
+                </div>
+                <div className={styles.rightSide}>
+                    <AnnotationCanvas group={points.label} points={points} setPoints={setPoints}/>
+                </div>
             </div>
 
-            <div className={styles.rightSide}>
-                <AnnotationCanvas group={points.label} points={points} setPoints={setPoints}/>
+            <div className={styles.bottomBtns}>
+                <button className={styles.submitBtn}>Submit</button>
+                <button className={styles.resetBtn}>Reset</button>
             </div>
-
         </div>
     )
 }
