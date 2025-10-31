b7477e7702ac2c68649fab08858df938ad0fd7a1
Kayed Mankal <103891908+kayedm@users.noreply.github.com>
2025-10-31 02:10:29 -0400

    styles: added styles

--- client/src/components/main/picture-annotation/AnnotationCanvas.jsx
+++ client/src/components/main/picture-annotation/AnnotationCanvas.jsx
@@ -1,14 +1,15 @@
 import AnnotateCard from './AnnotationCard';
+import styles from '../../styles/picture-annotation/AnnotationCanvas.module.css'
 
 export default function AnnotationCanvas({points, setPoints}) {
 
     console.log(points);
     return (
-        <div>
+        <div className={styles.container}>
             {
                 points.map((point) => (
-                <AnnotateCard key={point.id} point={point} setPoints={setPoints}/>
-            ))}
+                    <AnnotateCard key={point.id} point={point} setPoints={setPoints}/>
+                ))}
         </div>
     )
 }
