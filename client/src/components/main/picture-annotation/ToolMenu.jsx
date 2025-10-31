a5461d86c4bef9e29f81790551c4c1107bd4f4ad
Kayed Mankal <103891908+kayedm@users.noreply.github.com>
2025-10-31 02:16:08 -0400

    feat: added hide point

--- client/src/components/main/picture-annotation/ToolMenu.jsx
+++ client/src/components/main/picture-annotation/ToolMenu.jsx
@@ -1,6 +1,6 @@
 import styles from '../../styles/picture-annotation/ToolMenu.module.css'
 
-export default function ToolMenu({ setSelectedTool, selectedTool  }) {
+export default function ToolMenu({setHidePoints, setSelectedTool, selectedTool}) {
     const tools = ["Person", "Product", "Object", "Animal"];
 
     return (
@@ -9,14 +9,11 @@ export default function ToolMenu({ setSelectedTool, selectedTool  }) {
                 <button
                     key={tool}
                     onClick={() => setSelectedTool(tool)}
-                    className={selectedTool == tool ? styles.active : ""}
-                >
+                    className={selectedTool === tool ? styles.active : ""}>
                     {tool}
                 </button>
-
-
             ))}
-            <button className={styles.hideBtn}> Hide Points</button>
+            <button className={styles.hideBtn} onClick={ () => setHidePoints((prev) => !prev)}> Hide Points</button>
         </div>
     );
 }
