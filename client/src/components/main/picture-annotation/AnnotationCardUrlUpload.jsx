43be5c506666585a14506895dec79955c2269fd2
Kayed Mankal <103891908+kayedm@users.noreply.github.com>
2025-11-01 04:11:33 -0400

    feat: added the annotation card url upload component

---
+++ client/src/components/main/picture-annotation/AnnotationCardUrlUpload.jsx
@@ -0,0 +1,25 @@
+import styles from "./picture-annotation/AnnotationCardUrlUpload.module.css";
+import React from "react";
+
+export default function AnnotationCardUrlUpload({setUrlInput, urlInput, setImagePreview}) {
+
+    function handleImageUrl() {
+        const trimmed = urlInput.trim();
+        if (!trimmed) return;
+        setImagePreview(prev => [...prev, trimmed])
+        setUrlInput("");
+    }
+
+    return (
+        <div className={styles.uploadUrl}>
+            <input
+                type="text"
+                onChange={e => setUrlInput(e.target.value)}
+                value={urlInput}
+                className={styles.uploadUrlInput}
+            />
+            <button className={styles.uploadBtn} onClick={handleImageUrl}> Upload Url</button>
+        </div>
+    )
+
+}
