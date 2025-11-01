c471af7a36ed2fe4f806fbd2b0ca8ca25712d825
Kayed Mankal <103891908+kayedm@users.noreply.github.com>
2025-11-01 04:07:54 -0400

    feat: added annotation information component responsible for showing the annotation information including title, label and delete button

---
+++ client/src/components/main/picture-annotation/AnnotationCardInfo.jsx
@@ -0,0 +1,22 @@
+import styles from "./picture-annotation/AnnotationCardInfo.module.css";
+import {Delete, Shirt} from "lucide-react";
+
+export default function AnnotationCardInfo({point, setPoints }) {
+
+    function handleTitleChange(e) {
+        const title = e.target.value;
+        if (!title) return;
+        setPoints((prev) => prev.map((p => (p.id === point.id ? {...p, title} : p))))
+    }
+
+    return (
+        <div className={styles.info}>
+            <span><Shirt size={20}/></span>
+            <div className={styles.text}>
+                <div className={styles.label}>{point.label}</div>
+                <input onChange={e => handleTitleChange(e)} placeholder="Title"></input>
+            </div>
+            <button className={styles.deleteBtn} onClick={() => setPoints((prev) => prev.filter((p) => p.id !== point.id))}><Delete/></button>
+        </div>
+    )
+}
