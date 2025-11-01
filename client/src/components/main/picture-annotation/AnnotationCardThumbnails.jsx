b00b0fd5f4f38b91c2ec13b2ab054863c63742a4
Kayed Mankal <103891908+kayedm@users.noreply.github.com>
2025-11-01 04:10:59 -0400

    feat: added annotation card thumbnails component

---
+++ client/src/components/main/picture-annotation/AnnotationCardThumbnails.jsx
@@ -0,0 +1,45 @@
+import styles from "./picture-annotation/AnnotationCardPhotos.module.css";
+import {PlusIcon} from "lucide-react";
+import React, {useState} from "react";
+import AnnotationCardCrop from "./AnnotationCardCrop.jsx";
+
+export default function AnnotationCardThumbnails({setImagePreview, imagePreview}) {
+    const [showCrop, setShowCrop] = useState(false);
+    const [selectedImage, setSelectedImage] = useState(null);
+
+    function handleImageUpload(e) {
+        const file = e.target.files[0];
+        if (!file) return;
+        const url = URL.createObjectURL(file);
+        setImagePreview(prev => [...prev, url]);
+    }
+
+    function handleImageClick(image) {
+        setSelectedImage(image);
+        setShowCrop(true);
+    }
+
+    return (
+        <div className={styles.photos}>
+
+            {showCrop && (
+                <div className={styles.crop}>
+                    <AnnotationCardCrop selectedImage={selectedImage} setShowCrop={setShowCrop} imagePreview={imagePreview} setImagePreview={setImagePreview}/>
+                </div>
+            )}
+
+            <label className={styles.uploadImg}>
+                <span className={styles.addIcon}><PlusIcon/></span>
+                <input
+                    type="file"
+                    accept="image/*"
+                    onChange={handleImageUpload}
+                />
+            </label>
+
+            {imagePreview.map((image) => (
+                <img src={image} className={styles.image} onClick={() => handleImageClick(image)}/>
+            ))}
+        </div>
+    )
+}
