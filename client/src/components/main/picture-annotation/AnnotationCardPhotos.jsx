33906b193765f5545be964c762c3a5521a9de504
Kayed Mankal <103891908+kayedm@users.noreply.github.com>
2025-11-01 04:09:03 -0400

    feat: added annotation card photos component responsible for rendering the reference photo thumbnails and the url upload input and button

---
+++ client/src/components/main/picture-annotation/AnnotationCardPhotos.jsx
@@ -0,0 +1,18 @@
+import styles from "./picture-annotation/AnnotationCardPhotos.module.css";
+import React, {useState} from "react";
+import {PlusIcon} from "lucide-react";
+import AnnotationCardUrlUpload from "./AnnotationCardUrlUpload.jsx";
+import AnnotationCardThumbnails from "./AnnotationCardThumbnails.jsx";
+
+export default function AnnotationCardPhotos() {
+    const [imagePreview, setImagePreview] = useState([]);
+    const [urlInput, setUrlInput] = useState("");
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.photosText}> Photos</div>
+            <AnnotationCardThumbnails setImagePreview={setImagePreview} imagePreview={imagePreview} />
+            <AnnotationCardUrlUpload setUrlInput={setUrlInput} urlInput={urlInput} setImagePreview={setImagePreview} />
+        </div>
+    )
+}
