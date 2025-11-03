6152d05e832c01d298039b594b3709a2aa7d792e
Kayed Mankal <103891908+kayedm@users.noreply.github.com>
2025-11-03 13:03:20 -0500

    feat: added collapse icon to the annotation cards that hides the photos and url upload components

--- client/src/components/main/picture-annotation/AnnotationCardPhotos.jsx
+++ client/src/components/main/picture-annotation/AnnotationCardPhotos.jsx
@@ -2,16 +2,23 @@ import styles from "./styles/AnnotationCardPhotos.module.css";
 import React, {useState} from "react";
 import AnnotationCardUrlUpload from "./AnnotationCardUrlUpload.jsx";
 import AnnotationCardThumbnails from "./AnnotationCardThumbnails.jsx";
+import {ArrowDown} from "lucide-react";
 
 export default function AnnotationCardPhotos() {
     const [imagePreview, setImagePreview] = useState([]);
+    const [collapsed, setCollapsed] = useState(false);
     const [urlInput, setUrlInput] = useState("");
 
     return (
         <div className={styles.container}>
-            <div className={styles.photosText}> Photos</div>
-            <AnnotationCardThumbnails setImagePreview={setImagePreview} imagePreview={imagePreview} />
-            <AnnotationCardUrlUpload setUrlInput={setUrlInput} urlInput={urlInput} setImagePreview={setImagePreview} />
+            <div className={styles.photosText}>
+                Photos
+                <span className={styles.icon} onClick={() => setCollapsed(!collapsed)}><ArrowDown size={12}/></span>
+            </div>
+            <div className={`${styles.contentWrapper} ${collapsed ? styles.hidden : ""}`}>
+                <AnnotationCardThumbnails setImagePreview={setImagePreview} imagePreview={imagePreview} />
+                <AnnotationCardUrlUpload setUrlInput={setUrlInput} urlInput={urlInput} setImagePreview={setImagePreview} />
+            </div>
         </div>
     )
 }
