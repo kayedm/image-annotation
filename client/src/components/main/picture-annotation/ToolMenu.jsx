6c6167381f1686a2eaedcfea7095103d7cc780b0
Kayed Mankal <103891908+kayedm@users.noreply.github.com>
2025-11-03 13:12:36 -0500

    feat: added tools lookup function to set the icon dynamically

--- client/src/components/main/picture-annotation/ToolMenu.jsx
+++ client/src/components/main/picture-annotation/ToolMenu.jsx
@@ -1,16 +1,16 @@
 import styles from './styles/ToolMenu.module.css'
+import { tools } from '../data/tools.jsx'
 
 export default function ToolMenu({setHidePoints, setSelectedTool, selectedTool}) {
-    const tools = ["Person", "Product", "Object", "Animal"];
 
     return (
         <div className={styles.container}>
-            {tools.map((tool) => (
+            {tools.map(({label, icon: Icon }) => (
                 <button
-                    key={tool}
-                    onClick={() => setSelectedTool(prev => prev === tool ? null : tool)}
-                    className={selectedTool === tool ? styles.active : ""}>
-                    {tool}
+                    key={label}
+                    onClick={() => setSelectedTool(prev => prev === label ? null : label)}
+                    className={selectedTool === label ? styles.active : ""}>
+                    <Icon size={14}/>{label}
                 </button>
             ))}
             <button className={styles.hideBtn} onClick={ () => setHidePoints((prev) => !prev)}> Hide Points</button>
