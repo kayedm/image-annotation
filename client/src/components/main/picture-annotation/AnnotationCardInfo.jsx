043fafc24d9970d21b41fa380cc36e9e0e5b12a2
Kayed Mankal <103891908+kayedm@users.noreply.github.com>
2025-11-03 13:00:06 -0500

    styles: added tools look up function that returns an object of the selected tool. This is used to update the icons on the annotationcard to the proper ones.

--- client/src/components/main/picture-annotation/AnnotationCardInfo.jsx
+++ client/src/components/main/picture-annotation/AnnotationCardInfo.jsx
@@ -1,7 +1,8 @@
 import styles from "./styles/AnnotationCardInfo.module.css";
-import {Delete, Shirt, Trash2} from "lucide-react";
+import { toolLookup } from '../data/tools.jsx'
+import {Trash2} from "lucide-react";
 
-export default function AnnotationCardInfo({point, setPoints }) {
+export default function AnnotationCardInfo({point, setPoints}) {
 
     function handleTitleChange(e) {
         const title = e.target.value;
@@ -9,14 +10,22 @@ export default function AnnotationCardInfo({point, setPoints }) {
         setPoints((prev) => prev.map((p => (p.id === point.id ? {...p, title} : p))))
     }
 
+    const tool = toolLookup(point);
+    const Icon = tool.icon;
+
     return (
         <div className={styles.info}>
-            <span><Shirt size={20}/></span>
+            <span className={styles.icon}> <Icon size={20}/></span>
             <div className={styles.text}>
                 <div className={styles.label}>{point.label}</div>
                 <input onChange={e => handleTitleChange(e)} placeholder="Title"></input>
             </div>
-            <button className={styles.deleteBtn} onClick={() => setPoints((prev) => prev.filter((p) => p.id !== point.id))}><Trash2 size={15} /></button>
+            <button
+                className={styles.deleteBtn}
+                onClick={() => setPoints((prev) => prev.filter((p) => p.id !== point.id))}
+            >
+                <Trash2 size={15} />
+            </button>
         </div>
     )
 }
