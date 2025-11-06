561abba4e3e404d2f75c0858e0687f3034b431c0
Kayed Mankal <103891908+kayedm@users.noreply.github.com>
2025-11-06 10:31:27 -0500

    refactor: add EditingArea component and move props into their own objects

--- client/src/components/main/picture-annotation/PictureAnnotation.jsx
+++ client/src/components/main/picture-annotation/PictureAnnotation.jsx
@@ -3,26 +3,24 @@ import ToolMenu from "./ToolMenu.jsx";
 import styles from "./styles/PictureAnnotation.module.css";
 import React from "react";
 import AnnotationPanel from "./AnnotationPanel.jsx";
+import EditingArea from "./EditingArea.jsx";
 
 export default function PictureAnnotation() {
     const [selectedTool, setSelectedTool] = React.useState(null);
     const [hidePoints, setHidePoints] = React.useState(false);
     const [points, setPoints] = React.useState([]);
 
+    const toolState = { selectedTool, setSelectedTool };
+    const pointState = { points, setPoints, hidePoints, setHidePoints };
+
     return (
         <div className={styles.container}>
             <div className={styles.editingArea}>
                 <div className={styles.leftSide}>
-                        <div className={styles.toolMenu}>
-                            <ToolMenu  setHidePoints={setHidePoints} selectedTool={selectedTool} setSelectedTool={setSelectedTool}/>
-                        </div>
-                        <div className={styles.imagePreview}>
-                            <ImagePreview  hidePoints={hidePoints} points={points} setPoints={setPoints} selectedTool={selectedTool}/>
-                        </div>
+                    <EditingArea toolState={toolState} pointState={pointState} />
                 </div>
-
                 <div className={styles.rightSide}>
-                    <AnnotationPanel group={points.label} points={points} setPoints={setPoints}/>
+                    <AnnotationPanel pointState={pointState}/>
                 </div>
             </div>
         </div>
