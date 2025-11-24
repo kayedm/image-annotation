fbd92025181f64c05e90081bc0d26de10102a4b7
Kayed Mankal <103891908+kayedm@users.noreply.github.com>
2025-11-24 11:08:43 -0500

    refactor: - move header, content, and footer into the sidebar component - add imports for the Sidebar header, content, button, footer, icons, and react import

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,9 +1,27 @@
 import styles from "./styles/Sidebar.module.css";
+import SidebarHeader from "./SidebarHeader.jsx";
+import {Flame} from "lucide-react";
+import SidebarContent from "./SidebarContent.jsx";
+import SidebarButton from "./SidebarButton.jsx";
+import SidebarFooter from "./SidebarFooter.jsx";
+import React from "react";
 
-export default function Sidebar({ children, collapsed }) {
+export default function Sidebar({ onClose, setOnClose }) {
     return (
-        <aside className={styles.sidebar} data-collapsed={collapsed}>
-            {children}
-        </aside>
+        <div className={styles.container}>
+            <aside className={styles.sidebar} >
+                <div className={styles.header}>
+                    <SidebarHeader logo={<Flame/>} onClose={onClose} setOnClose={setOnClose}/>
+                </div>
+                <div className={styles.content}>
+                    <SidebarContent>
+                        <SidebarButton to="task-page" > List </SidebarButton>
+                    </SidebarContent>
+                </div>
+                <div className={styles.footer}>
+                    <SidebarFooter username="Test User" role="Contributer"/>
+                </div>
+            </aside>
+        </div>
     );
 }
