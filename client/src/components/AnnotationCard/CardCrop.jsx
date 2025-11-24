bf7c93d8a17111ab284eaee4022df6f6de4ba699
Kayed Mankal <103891908+kayedm@users.noreply.github.com>
2025-11-24 16:13:50 -0500

    refactor: update the CardCrop component to use the annotation prop from the zustand store, remove unused props

---
+++ client/src/components/AnnotationCard/CardCrop.jsx
@@ -0,0 +1,18 @@
+import styles from "./styles/CardCrop.module.css";
+import {useRef, useState} from "react";
+import CardCropButtons from "./CardCropButtons.jsx";
+import CardCropPreview from "./CardCropPreview.jsx";
+
+export default function CardCrop({annotation, selectedImage, setShowCrop}) {
+    const [cropPoints, setCropPoints] = useState([]);
+    const imgRef = useRef(null);
+
+    return (
+        <div className={styles.container} onClick={() => setShowCrop(false)}>
+            <div className={styles.wrapper} onClick={e => e.stopPropagation()}>
+                <CardCropPreview selectedImage={selectedImage} setCropPoints={setCropPoints} cropPoints={cropPoints} imgRef={imgRef} referenceImages={annotation.referenceImages} />
+                <CardCropButtons annotation={annotation} selectedImage={selectedImage} setShowCrop={setShowCrop} cropPoints={cropPoints} imgRef={imgRef} />
+            </div>
+        </div>
+    );
+}
