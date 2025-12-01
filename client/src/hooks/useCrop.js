51ff38b20be3ccaf48b8b0737ddf6731c6f85808
Kayed Mankal <103891908+kayedm@users.noreply.github.com>
2025-12-01 18:39:25 -0500

    feat: add useCrop hook for image cropping
    - sets crop points on click
    - updates the crop points on mouse move

---
+++ client/src/hooks/useCrop.js
@@ -0,0 +1,29 @@
+import {getLocalCoords} from "../utils/coords.js";
+
+export default function useCrop({didDrag, isCropping, setCropPoints, setIsCropping}) {
+
+    function handleClick(e) {
+        const cropPoint = getLocalCoords(e)
+        if (didDrag.current) return;
+        if (!isCropping) {
+            setCropPoints([cropPoint]);
+            setIsCropping(true)
+        }
+        if (isCropping) {
+            setCropPoints(prev => [prev[0], cropPoint]);
+            setIsCropping(false)
+        }
+    }
+
+    function handleMouseMove(e) {
+        if (!isCropping) return;
+        const cropPoint = getLocalCoords(e)
+        setCropPoints(prev => [prev[0], cropPoint]);
+    }
+
+
+    return {
+        handleClick,
+        handleMouseMove,
+    }
+}
